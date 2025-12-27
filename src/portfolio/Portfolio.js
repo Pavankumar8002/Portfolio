54f726f7a06a6372b2a4038295d3b793a8f90da4
Pavan kumar N <108813090+Pavankumar8002@users.noreply.github.com>
2025-12-27 18:31:06 +0530

    picture issue resolved

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -104,7 +104,7 @@ function Portfolio() {
       {/* PROFILE IMAGE */}
       <div className="col-md-5 text-center">
         <img
-          src="/Pictures/picture.jpeg"
+          src="/Pictures/profilepic.heic"
           alt="Profile"
           className="about-image img-fluid shadow"
         />
